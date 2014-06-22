c8d429783d80d246325f91b5f5d65ee9aecacd23
XiaokangWang <xiaokangwang@outlook.com>
2014-06-22 14:03:33 +0800

    Added config read.

--- KKUsbInO.py
+++ KKUsbInO.py
@@ -1,6 +1,7 @@
 __author__ = 'xiaokangwang'
 import os
 import shutil
+import json
 
 
 def enum_drive_win():
@@ -19,7 +20,25 @@ def enum_drive_linux(mounting_point):
     return set(os.listdir(mounting_point))
  
 def main():
-    pass
+    if !os.path.exists("push"):
+        print("nothing to push")
+
+    if !os.path.isdir("push"):
+        print("push should be a dir")
+
+    if !os.path.isfile("config.json"):
+        print "no configure found"
+
+    config = {}
+
+    try:
+        with open("config.json") as config_fd:
+            config = json.load(config_fd)
+    except Exception as errs:
+        print(err)
+
+
+
  
 if __name__ == "__main__":
     main()
