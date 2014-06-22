e4f613a64cf4970539fe1641cfc023ec11a8b4bb
XiaokangWang <xiaokangwang@outlook.com>
2014-06-22 13:30:49 +0800

    Added Enum drive in linux.

--- KKUsbInO.py
+++ KKUsbInO.py
@@ -1,17 +1,22 @@
 __author__ = 'xiaokangwang'
 import os
 import shutil
- 
-def enum_drive():
+
+
+def enum_drive_win():
     drivelst=set()
     chars="abcdefghijkmlnopqrstvuwxyz"
-    charlst=(chars)
+    charlst = list(chars)
     for char in charlst:
         res=os.system("cd "+char+":")
         if !res:
             drivelst.append(char)
 
- 
+    return drivelst
+
+
+def enum_drive_linux(mounting_point):
+    return set(os.listdir(mounting_point))
  
 def main():
     pass
