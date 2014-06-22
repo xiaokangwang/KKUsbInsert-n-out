8a3998ab6d98c1016eec93636bd2ff41f2eeb9ff
XiaokangWang <xiaokangwang@outlook.com>
2014-06-22 15:49:45 +0800

    Added Config editing.

--- KKUsbInOConfigs.py
+++ KKUsbInOConfigs.py
@@ -24,14 +24,19 @@ def main():
             print("config.conf must be file")
         else:
             lg.info("config.conf doesn't exist", stack_info=True)
-            config["uuid"] = str(uuid.uuid4())
+            config["uuid"] = str(uuid.uuid4())  # prevent loading json from a empty file, which will lead to exception
             print("config.conf doesn't exist, we will create one.")
+
+    print("Ready, uuid: %s", config["uuid"])
 
     while (1):
         lg.debug("requesting for next command")
         inpd = input("[>")
 
         inpds = inpd.split()
+
+        if len(inpds) == 0:
+            continue
 
         if inpds[0] == "x":
             lg.info("Asked to writing to file")
@@ -51,6 +56,34 @@ def main():
         if inpds[0] == "q":
             lg.info("Asking to quit")
             exit(0)
+
+        if inpds[0] == "r":
+            config = {}
+            config["uuid"] = str(uuid.uuid4())
+            lg.info("Asking to reset uuid, new uuid: %s", config["uuid"])
+            print("Reseted, uuid: ", config["uuid"])
+
+        if inpds[0] == "l":
+            lg.info("Asking to list config")
+            for key in config.keys():
+                print(key, config[key])
+            print("EOL")
+
+        if inpds[0] == "s":
+            if len(inpds) != 3:
+                print("too few or too many args")
+            config[inpds[1]] = inpds[2]
+            lg.info("set arg: %s => %s ", inpds[1], inpds[2])
+
+        if inpds[0] == "d":
+            if len(inpds) != 3:
+                print("too few or too many args")
+
+            print(config[inpds[1]])
+            lg.info("get arg: %s => %s ", inpds[1])
+
+
+
 
 
 if __name__ == "__main__":
